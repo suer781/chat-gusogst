917f2c06490242d34c0a41d20d9c921469480ee7
suer781 <suer781@users.noreply.github.com>
2026-06-08 18:34:12 +0000

    Disable Chaquopy plugin in root build

--- android-native/build.gradle.kts
+++ android-native/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application") version "8.10.1" apply false
     id("org.jetbrains.kotlin.android") version "2.4.0" apply false
-    id("com.chaquo.python") version "16.0.0" apply false
+    // Chaquopy disabled for initial build
+    // id("com.chaquo.python") version "16.0.0" apply false
 }
